5d51903d7a044d90f5c3de9f139e425cd6cba449
Diego Watanabe <dwatanabe314@gmail.com>
2022-11-05 02:00:01 -0400

    Add stg payments and stg orders to assets

--- jaffle/assets/__init__.py
+++ jaffle/assets/__init__.py
@@ -9,3 +9,20 @@ def stg_customers() -> SQL:
     )
     df.rename(columns={"id": "customer_id"}, inplace=True)
     return SQL("select * from $df", df=df)
+
+@asset
+def stg_orders() -> SQL:
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_orders.csv"
+    )
+    df.rename(columns={"id": "order_id", "user_id": "customer_id"}, inplace=True)
+    return SQL("select * from $df", df=df)
+
+@asset
+def stg_payments() -> SQL:
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_payments.csv"
+    )
+    df.rename(columns={"id": "payment_id"}, inplace=True)
+    df["amount"] = df["amount"].map(lambda amount: amount / 100)
+    return SQL("select * from $df", df=df)
