a9dfe9c8ec50479c9e22871ca49b2eb22321f5e2
Diego Watanabe <dwatanabe314@gmail.com>
2022-11-05 02:04:17 -0400

    Add sql query for orders

--- jaffle/assets/__init__.py
+++ jaffle/assets/__init__.py
@@ -80,3 +80,43 @@ select * from final
         stg_orders=stg_orders,
         stg_payments=stg_payments,
     )
+
+@asset
+def orders(stg_orders: SQL, stg_payments: SQL) -> SQL:
+    payment_methods = ["credit_card", "coupon", "bank_transfer", "gift_card"]
+    return SQL(
+        f"""
+with orders as (
+    select * from $stg_orders
+),
+payments as (
+    select * from $stg_payments
+),
+order_payments as (
+    select
+        order_id,
+        {"".join(f"sum(case when payment_method = '{payment_method}' then amount else 0 end) as {payment_method}_amount," for payment_method in payment_methods)}
+        sum(amount) as total_amount
+    from payments
+    group by order_id
+),
+final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        orders.status,
+
+        {"".join(f"order_payments.{payment_method}_amount," for payment_method in payment_methods)}
+
+        order_payments.total_amount as amount
+    from orders
+    left join order_payments
+        on orders.order_id = order_payments.order_id
+)
+
+select * from final
+    """,
+        stg_orders=stg_orders,
+        stg_payments=stg_payments,
+    )
