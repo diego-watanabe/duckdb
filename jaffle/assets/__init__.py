ae9eaf034c8b4e45d7aa93600792becfe63108d7
Diego Watanabe <dwatanabe314@gmail.com>
2022-11-05 01:58:49 -0400

    Add new asset for jaffle shop example, remove previous assets

--- jaffle/assets/__init__.py
+++ jaffle/assets/__init__.py
@@ -1,28 +1,11 @@
 from dagster import asset
-from jaffle.duckpond import SQL 
+from jaffle.duckpond import SQL
 import pandas as pd
 
-@asset 
-def population() -> SQL:
-    df = pd.read_html(
-        "https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
-    )[0]
-    df.columns = [
-        "country",
-        "continent",
-        "subregion",
-        "population_2018",
-        "population_2019",
-        "pop_change",
-    ]
-    df["pop_change"] = [
-        float(str(row).rstrip("%").replace("\u2212", "-")) for row in df["pop_change"]
-    ]
-    return SQL("select * from $df", df=df)
-
 @asset
-def continent_population(population: SQL) -> SQL:
-    return SQL(
-        "select continent, avg(pop_change) as avg_pop_change from $population group by 1 order by 2 desc",
-        population=population,
+def stg_customers() -> SQL:
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_customers.csv"
     )
+    df.rename(columns={"id": "customer_id"}, inplace=True)
+    return SQL("select * from $df", df=df)
